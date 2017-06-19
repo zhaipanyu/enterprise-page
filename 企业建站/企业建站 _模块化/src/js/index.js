53bc4f06d72d72478c0053ec8759c16ce764b13a
zhaizhai <1044076780@qq.com>
2017-06-19 22:26:56 +0800

    add

--- 企业建站/企业建站 _模块化/src/js/index.js
+++ 企业建站/企业建站 _模块化/src/js/index.js
@@ -16,27 +16,27 @@ define(['jquery'], function () {
             }
         }
 
-        // $('.support').on('click', function (e) {
-        //     e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
-        //     var x = $('.services').offset().top;//550
-        //     $(window).scrollTop(x - 70);
-        // })
+        $('.support').on('click', function (e) {
+            e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
+            var x = $('.services').offset().top;//550
+            $(window).scrollTop(x - 70);
+        })
 
-        // $('.introduce').on('click', function (e) {
-        //     e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
-        //     var x = $('.introduce1').offset().top;//550
-        //     $(window).scrollTop(x - 70);
-        // })
-        // $('.about').on('click', function (e) {
-        //     e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
-        //     var x = $('.about1').offset().top;//550
-        //     $(window).scrollTop(x - 70);
-        // })
-        // $('.products').on('click', function (e) {
-        //     e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
-        //     var x = $('.products1').offset().top;//550
-        //     $(window).scrollTop(x - 70);
-        // })
+        $('.introduce').on('click', function (e) {
+            e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
+            var x = $('.introduce1').offset().top;//550
+            $(window).scrollTop(x - 70);
+        })
+        $('.about').on('click', function (e) {
+            e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
+            var x = $('.about1').offset().top;//550
+            $(window).scrollTop(x - 70);
+        })
+        $('.products').on('click', function (e) {
+            e.preventDefault();//不设置这个,会回到顶部,会跟随冒泡事件!
+            var x = $('.products1').offset().top;//550
+            $(window).scrollTop(x - 70);
+        })
 
 
         return {
